fae91ef7e8f57b8c18c411a00ff1a890069d540c
Musiab <musaibjeelani999@gmail.com>
2024-12-16 19:39:31 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/musaibmirrr/ReactToDoList.git', // Replace 'your-repo-name' with your GitHub repository name
+  base: '/https://github.com/musaibmirrr/ReactToDoList.git/', // Replace 'your-repo-name' with your GitHub repository name
 });
